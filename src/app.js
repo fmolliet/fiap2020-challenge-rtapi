24d538f17b9889e49457e75fb5d2ed3e16d0fbe0
fmolliet <fmolliet@hotmail.com>
2020-08-23 23:35:36 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors    = require('cors');
+const morgan  = require('morgan');
+const dotenv  = require('dotenv').config();
+const logger  = require('./utils/logger');
+
+require('./database/connect');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.post('/cart', (req, res)=>{
+    logger.info(req.body);
+});
+
+app.post('/recommendation', (req, res)=>{
+    logger.info(req.body);
+});
+
+module.exports = app;
